a77c58099e1c815e1328040cd78da81d59c8e2ef
Camila Taborda <camilatabordach@gmail.com>
2024-08-16 10:28:52 -0300

    commit

--- VersãoProfessor2.py
+++ VersãoProfessor2.py
@@ -1,10 +1,14 @@
 # Leia um nome e escreva apenas as vogais do nome - diga quantas vogais 
 texto = input('Qual seu nome?')
 tam = len(texto)
-
+vogais = ''
 indice = 0
+num_vogais = 0
 while indice < tam:
     letra = texto[indice]
     if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
-        print(texto[indice])
+        vogais = vogais + letra #concatenando 
+        num_vogais = num_vogais +1
     indice += 1 
+
+print(vogais, num_vogais, {100*num_vogais/tam})
