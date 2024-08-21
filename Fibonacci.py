b8603bffc664b654ab195fc41e29e6e31b158e8a
Camila Taborda <camilatabordach@gmail.com>
2024-08-21 10:46:48 -0300

    Fibonacci

--- Fibonacci.py
+++ Fibonacci.py
@@ -3,15 +3,14 @@
 #termo é a soma dos dois termos anteriores. Por exemplo, para n=8 escreva 0, 1, 1, 2,
 #3, 5, 8 e 13.
 n = int(input("Digite o valor de n: "))
-a = 0
-b = 1
-count = 0
+fib1 = 0
+fib = 1
+cont = 0
 print("Sequência de Fibonacci:")
-while count < n:
-    print(a, end=" ")
-    temp = a
-    a = b
-    b = temp + b
-    
-    count += 1
+while cont < n:
+    print(fib1, end=" ")
+    fib2 = fib1 #armazendo a em uma variavel temporaria
+    fib1 = fib #a vai receber o valor b
+    fib = fib2 + fib # b vai receber o valor da variavel temporaria mais b
+    cont += 1
 
